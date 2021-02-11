d70e6cc5dbd3fa25aec13039c0b68043a866f747
Orok Ironbar <rokkside@gmail.com>
2021-02-11 21:27:01 +0000

    commiting master

---
+++ LevelUpTeam/linux.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Please enter your name:"
+read name
+echo "Reading.... please wait"
+sleep 3 
+echo "-----------------------------"
+echo "Your username is:" && whoami 
+echo "-----------------------------"
+echo "This is one of my first Linux Scripts"
+sleep 2
+echo "Please wait while we process the date......."
+sleep 3
+echo "Made on:" && date
+echo "-----------------------------"
+sleep 2
+echo "My IP address is:"
+echo "-----------------------------"
+sleep 2
+curl ifconfig.me
+
+add a new line
+add anoter line
+add a test env
